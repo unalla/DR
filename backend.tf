8a073f3137f4c72ce7e2f64127b827e7941064e6
ubnta <udhaya.nallamottu@gmail.com>
2026-01-17 15:32:27 -0600

    updated backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ec2_dr_failover_destroy"
+    bucket         = "ec2-dr-failover-destroy"
     key            = "global/s3/terraform.tfstate" # The file path in the bucket
     region         = "us-east-1"
     dynamodb_table = "ec2-dr-dynamo"
